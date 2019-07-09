a95515d5d323f00873b81226b614ce097527667e
Pierre Chifflier <chifflier@wzdftpd.net>
2019-07-09 10:13:08 +0200

    Commit benchmarks

---
+++ benches/b_krb5_parser.rs
@@ -0,0 +1,26 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+
+extern crate nom;
+extern crate kerberos_parser;
+
+use kerberos_parser::krb5_parser::*;
+
+
+static KRB5_TICKET: &'static [u8] = include_bytes!("../assets/krb5-ticket.bin");
+
+#[bench]
+fn bench_parse_ticket(b: &mut Bencher) {
+    b.iter(|| {
+        let res = parse_krb5_ticket(KRB5_TICKET);
+        match res {
+            Ok((rem,tkt)) => {
+                assert!(rem.is_empty());
+                assert_eq!(tkt.tkt_vno, 5);
+            },
+            _ => assert!(false),
+        }
+    });
+}
